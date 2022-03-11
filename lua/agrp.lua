6cd65875cfe03999ae9341a37847777bdde4217b
delphinus <me@delphinus.dev>
2022-03-11 13:22:16 +0900

    Add explanation for each section

--- lua/agrp.lua
+++ lua/agrp.lua
@@ -9,15 +9,23 @@ local function manage_definitions(definitions, group)
     vim.validate {
       definition = { definition, "table" },
     }
+    -- When group is nil, it does not set augroup.
     local opt = { group = group }
     local event
     local cb_or_cmd
     if type(key) == "number" then
+      -- Each definition has all params to set.
+      -- {
+      --   {'TextYankPost', ...},
+      --   {'VimEnter', ...},
+      -- }
       if #definition == 3 then
+        -- ex. {'TextYankPost', '*', function() vim.highlight.on_yank{} end},
         opt.pattern = definition[2]
         event = definition[1]
         cb_or_cmd = definition[3]
       elseif #definition == 4 then
+        -- ex. {'VimEnter', '*', {'once'}, function() vim.cmd[[echo 'Hello, World!']] end},
         opt.once = definition[1].once and true or false
         opt.nested = definition[1].nested and true or false
         opt.pattern = definition[3]
@@ -27,6 +35,13 @@ local function manage_definitions(definitions, group)
         error "each definition should have 3 values (+options (once, nested))"
       end
     else
+      -- One event has many definitions
+      -- {
+      --   ['BufNewFile,BufRead'] = {
+      --     {'*.hoge', 'set filetype=hoge'},
+      --     {'*.fuga', 'set filetype=fuga'},
+      --   },
+      -- }
       for _, d in ipairs(definition) do
         if #d == 2 or #d == 3 then
           opt.pattern = d[1]
@@ -53,9 +68,23 @@ M.set = function(groups)
       definitions = { definitions, "table" },
     }
     if type(name) == "number" then
+      -- This section deals with the pattern that has no augroup.
+      --   require'agrp'.set{
+      --     {
+      --       ['BufNewFile,BufRead'] = {
+      --         {'*.hoge', 'set filetype=hoge'},
+      --       },
+      --     },
+      --   }
       manage_definitions(definitions)
     else
-      vim.api.nvim_create_augroup ( name,{} )
+      -- This is for the usual pattern that has augroup.
+      --   require'agrp'.set{
+      --     MyFavorites = {
+      --       {'QuickFixCmdPost', '*grep*', 'cwindow'},
+      --     },
+      --   }
+      vim.api.nvim_create_augroup(name, {})
       manage_definitions(definitions, name)
     end
   end
